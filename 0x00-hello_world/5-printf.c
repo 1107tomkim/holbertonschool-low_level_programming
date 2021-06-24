3ca857cd9b6f0322857bc7f200af25dba77d46f8
1107tomkim <3268@holbertonschool.com>
2021-06-24 04:44:31 +0000

    Betty Fixes

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Write a C program that prints exactly with proper grammar,
+ * but the outcome is a piece of art, follwed by a new line
+ * Return: 0, if everything executes properly
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
