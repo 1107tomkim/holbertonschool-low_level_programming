250f0aadef63fc3609b08b11f9ff729f7523383b
1107tomkim <3268@holbertonschool.com>
2021-06-24 04:42:54 +0000

    Betty Fixes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-/*
+/**
  * main - Prints \"Programming is like building a multilingual
  * Return: 0 if everything executed properly
  */
-main(void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
