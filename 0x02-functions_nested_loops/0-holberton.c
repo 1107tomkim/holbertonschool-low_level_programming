08827e84aaf353252f9cac76cba191c9f4aea9d1
1107tomkim <3268@holbertonschool.com>
2021-06-28 20:52:32 +0000

    Program to print Holberton followed by new line

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Program to prin Holberton followed by new line
+ * Return:0, if everything executes properly.
+ */
+
+int main(void)
+{
+	printf("Holberton\n");
+	return(0);
+}
