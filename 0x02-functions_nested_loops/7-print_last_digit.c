3dcdc9fc3b0ff431416973c3dfd31b191229dd47
1107tomkim <3268@holbertonschool.com>
2021-06-29 19:25:49 +0000

    Betty format fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,12 +4,13 @@
  * @n: the in value that is being tested
  *
  * Return: 0 on success,
- */ 
+ */
 int print_last_digit(int n)
 {
 	int last_digit = n % 10;
-	if(last_digit < 0)
-		last_digit *=-1;
+
+	if (last_digit < 0)
+		last_digit *= -1;
 	_putchar(last_digit + '0');
 	return (last_digit);
 }
