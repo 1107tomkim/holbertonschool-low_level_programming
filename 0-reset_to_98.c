5aae04ae575aa8de23cf9981a11c611eba6edd03
1107tomkim <cryptotaekim@gmail.com>
2021-07-07 08:27:23 -0700

    Function to take pointer to an int and sets value to 98

---
+++ 0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - Function that takes a pointer to an int
+ * as parameter and updates the value it points to 98
+ * @n: the value that is being inputted and tested
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
