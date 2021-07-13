f5cf6d1279c0d9f95251e5c9de265657e6b5345c
1107tomkim <3268@holbertonschool.com>
2021-07-13 03:50:54 +0000

    Holberton header file

---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pot_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+
+#endif
