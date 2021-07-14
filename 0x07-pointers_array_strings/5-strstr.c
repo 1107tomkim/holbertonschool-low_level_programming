1ea2481ce1241ae4205999780cf217308d1badc2
1107tomkim <3268@holbertonschool.com>
2021-07-14 02:51:56 +0000

    Function that locates a substring

---
+++ 0x07-pointers_array_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * _strstr - Function that locates a substring
+ * @haystack: Array thats being tested
+ * @needle: Tester
+ * Return: 0, if everyone executes properly.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+
+	if (need[0] == '\0')
+		return (haystack);
+
+	for (i = 0; haystack[i]; i++)
+	{
+		for (j = 0; needle[j]; j++)
+		{
+			if (needle[i + j] == '\0')
+				return (haystack + i);
+			if (haystack[i + j] != needle[j])
+				break;
+		}
+	}
+	return ('\0');
+}
