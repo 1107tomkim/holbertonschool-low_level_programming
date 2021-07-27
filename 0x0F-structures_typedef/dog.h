ff6e73f55dad7d477916f0f86898b04d39cdbeae
1107tomkim <3268@holbertonschool.com>
2021-07-27 02:58:46 +0000

    Dog.h header file

---
+++ 0x0F-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - A simple struct including
+ * few elements that are provided.
+ * @name: Contains name in char array.
+ * @age: Contains age in int including decimals.
+ * @owner: Contains name in char array.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
