d16a453c2109693e37fe1536fbcefefcd5778342
1107tomkim <3268@holbertonschool.com>
2021-07-28 04:11:46 +0000

    Header typo and syntax fixes

--- 0x10-function_pointers/0-print_name.c
+++ 0x10-function_pointers/0-print_name.c
@@ -1,6 +1,6 @@
 #include "function_pointers.h"
 #include <stdlib.h>
-#include <stdhio.h>
+#include <stdio.h>
 /**
  * print_name -  function that prints
  * a name.
@@ -10,6 +10,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+
 	void (*ptr)(char *);
 
 	if (name == NULL || f == NULL)
