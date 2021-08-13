1a580fbf987efde84510314b719ed969f1567399
1107tomkim <3268@holbertonschool.com>
2021-08-13 14:33:58 +0000

    Function that converts binary num to unsigned int

---
+++ 0x15-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ *
+ *
+ *
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i = 0;
+	unsigned int j = 1;
+	unsigned int k = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[i + 1] != '\0')
+	{
+		i++;
+	}
+	while (i >= 0)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		k += (b[i] - '0') * j;
+		j *= 2;
+		i--;
+	}
+	return (k);
+}
+
+
