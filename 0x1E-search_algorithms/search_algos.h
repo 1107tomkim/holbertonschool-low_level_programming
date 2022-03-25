0833806436d8a8c7242aa461a7c355e2ad211a78
1107tomkim <cryptotaekim@gmail.com>
2022-03-25 04:14:12 +0000

    Header including the function matching with the advanced_binary

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,9 +1,13 @@
-#ifndef SEARCH_ALGOS_H
-#define SEARCH_ALGOS_H
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+int bhelper(int *array, int value, size_t low, size_t high);
+void print_array(int *array, size_t low, size_t high);
+
 
 #endif
