e6a231031ecc3377d8758d76d923c054a939d744
1107tomkim <cryptotaekim@gmail.com>
2022-04-04 02:38:04 +0000

    Header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,9 +5,27 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ */
+typedef struct skiplist_s
+{
+    int n;
+    size_t index;
+    struct skiplist_s *next;
+    struct skiplist_s *express;
+} skiplist_t;
+
 int advanced_binary(int *array, size_t size, int value);
 int bhelper(int *array, int value, size_t low, size_t high);
 void print_array(int *array, size_t low, size_t high);
-
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
 #endif
